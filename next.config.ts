255e7231c2619de630edb438701f9f7f96ade715
Louis <louisleonid325@gmail.com>
2026-01-01 19:45:14 +0700

    chore: remove conflicting PostHog rewrites

--- next.config.ts
+++ next.config.ts
@@ -11,18 +11,7 @@ const nextConfig: NextConfig = {
       ],
     },
   ],
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
